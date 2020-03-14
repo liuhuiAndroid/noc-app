fb2248e32648543c0edd0b5037a5ce1c3a2edbe0
LiuHuiAndroid <642404044@qq.com>
2020-03-14 13:43:24 +0800

    图文详情页面搭建

---
+++ app/src/main/java/com/noc/app/ui/activity/FeedDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.noc.app.ui.activity
+
+import android.os.Bundle
+import com.noc.lib_common_ui.base.BaseActivity
+
+class FeedDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
